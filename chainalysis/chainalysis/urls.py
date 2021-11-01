698b058d10b3286a13458764df71e588fee7e81f
Ethan Evans <ethanevans622@gmail.com>
2021-11-01 12:10:21 -0500

    exchange_prices: loaded coins from two exchanges

--- chainalysis/chainalysis/urls.py
+++ chainalysis/chainalysis/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from exchange_prices import views
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
 ]
+urlpatterns += staticfiles_urlpatterns()
+
--- chainalysis/exchange_prices/views.py
+++ chainalysis/exchange_prices/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tools.price_fetcher import fetch_books
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html') 
+    context_info = {}
+    coins = []
+    exchanges = ("coinbasepro", "gemini")
+    coins.append(get_coin("BTC", exchanges))
+    coins.append(get_coin("ETH", exchanges))
+    context_info["coins"] = coins
+    return render(request,'exchange.html', context=context_info)
+
+#Helper functions (NOT VIEWS)
+def get_coin(coin, exchanges, quote="USD", count=5):
+    books = []
+    for exchange in exchanges:
+        books.append(fetch_books(exchange, coin, quote, count))
+    return {"name": coin, "books": books}
+
