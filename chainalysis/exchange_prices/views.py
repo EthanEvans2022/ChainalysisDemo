3471d02fe65c484f03210f6015a9728fb7d9c8dd
Ethan Evans <ethanevans622@gmail.com>
2021-11-01 19:34:42 -0500

    exchange_price: basic recommendation logic

--- chainalysis/exchange_prices/views.py
+++ chainalysis/exchange_prices/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tools.price_fetcher import fetch_books
+from tools.price_fetcher import fetch_books, calculate_recommendations
 
 # Create your views here.
 def home(request):
     context_info = {}
     coins = []
     exchanges = ("coinbasepro", "gemini")
-    coins.append(get_coin("BTC", exchanges))
-    coins.append(get_coin("ETH", exchanges))
+    print("getting BTC")
+    coins.append(get_coin("BTC", exchanges, count=3))
+    print("getting ETH")
+    coins.append(get_coin("ETH", exchanges, count=3))
+    print("Got all coins")
     context_info["coins"] = coins
     return render(request,'exchange.html', context=context_info)
 
 #Helper functions (NOT VIEWS)
 def get_coin(coin, exchanges, quote="USD", count=5):
-    books = []
+    exchange_books = []
     for exchange in exchanges:
-        books.append(fetch_books(exchange, coin, quote, count))
-    return {"name": coin, "exchanges": books}
+        print("foo 1")
+        exchange_books.append(fetch_books(exchange, coin, quote, count))
+    buy_rec, sell_rec = calculate_recommendations(exchange_books)
+    return {"name": coin, "exchanges": exchange_books, "recommendation": (buy_rec, sell_rec)}
 
--- chainalysis/tools/price_fetcher.py
+++ chainalysis/tools/price_fetcher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import shrimpy
 import json
+import math
 from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
@@ -9,10 +10,6 @@ public_key="4120ad4bc0a5e079bd1bebdca2d43d531074bb8605ed334a5ab81a5f651469d7"
 #TO-DO: GET KEY HERE WHILE ENCRYPTED if have time
 secret_key="DUMMY123"
 client = shrimpy.ShrimpyApiClient(public_key, secret_key)
-
-def fetch_books(exchange, baseSymbol=None, quoteSymbol=None, limit=5):
-    orderbooks =  client.get_orderbooks(exchange, baseSymbol, quoteSymbol, limit)[0]#returns list of dicts
-    return filter_raw_books(orderbooks)
 
 def filter_raw_books(raw_books):
     cleaned_books = {}
@@ -24,6 +21,50 @@ def filter_raw_books(raw_books):
     books.append({"name": "Bids", "offers": raw_books["orderBooks"][0]["orderBook"]["bids"]})
     cleaned_books["books"] = books
     return cleaned_books
+
+def fetch_books(exchange, baseSymbol=None, quoteSymbol=None, limit=5):
+    print("fetching book")
+    orderbooks =  client.get_orderbooks(exchange, baseSymbol, quoteSymbol, limit)[0]#returns list of dicts
+    print("orderbooks:")
+    print(orderbooks)
+    return filter_raw_books(orderbooks)
+
+def calculate_recommendations(exchanges):
+    '''
+    have list of exchanges
+        name
+        books[]
+            name
+            offers[]
+                price: str
+                quantity: str
+
+    track best buy/sell price at each exchange
+    loop through all exchanges and get the best
+    '''
+    print("finding recommendation")
+    return recur_recommendation(exchanges)
+
+def recur_recommendation(exchanges):
+    print("help")
+    best_ask = (math.inf, "ERROR")
+    best_bid = (-math.inf, "ERROR")
+    if len(exchanges) == 1:
+        name = exchanges[0]["name"]
+        ask = exchanges[0]["books"][0]["offers"][0]
+        bid = exchanges[0]["books"][1]["offers"][0]
+        return (ask, name), (bid, name)
+
+    midpoint = int(len(exchanges)/2)
+    left = exchanges[:midpoint]
+    right = exchanges[midpoint:]
+    best_ask_left, best_bid_left = recur_recommendation(left)
+    best_ask_right, best_bid_right = recur_recommendation(right)
+
+    best_ask = best_ask_left if best_ask_left[0]["price"] == min(best_ask_left[0]["price"], best_ask_right[0]["price"]) else best_ask_right
+    best_bid = best_bid_right if best_bid_left[0]["price"] == max(best_bid_left[0]["price"], best_bid_right[0]["price"]) else best_bid_right
+    return best_ask, best_bid
+
 
 def print_exchanges():
     supported_exchanges = client.get_supported_exchanges()
